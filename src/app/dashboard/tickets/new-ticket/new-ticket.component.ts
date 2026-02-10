6b2406865fa73cd73b46099effe22be1df284fa9
jovan.petrovic <jovanloki@gmail.com>
2026-02-10 14:41:06 +0100

    Gained access to form element via ViewChild. Gained access to form and form children using # operator

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(titleInput: string, requestInput: string) {
+    console.log('Submitted:', titleInput, requestInput);
+    this.form?.nativeElement.reset();
+  }
+}
